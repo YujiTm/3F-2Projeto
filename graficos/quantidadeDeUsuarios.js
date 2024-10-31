ef9f300fa109d9d64f89b6c44859bae506654aa8
YujiTm <128611437+YujiTm@users.noreply.github.com>
2024-10-31 09:23:38 -0300

    31/10

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./comum.js";
+import { getCSS, tickfont } from "./comum.js";
 async function quantidadeDeUsuarios(){
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url);
@@ -24,9 +24,18 @@ async function quantidadeDeUsuarios(){
         text: 'Redes socias com mais usuários no mundo',
         x: 0,
         font:{
-          color getCSS('--cor-primaria'),
+          color: getCSS('--cor-primaria'),
           family: getCSS('--font'),
           size:30
+        }
+      },
+      xaxis:{
+        tickfont: tickfont,
+        title: {
+          text:'Nome das redes sociais',
+          font:{
+            color: getCSS('--cor-secundaria')
+          }
         }
       }
     }
